187844e212205a367d4774932bf15124c5bb7d01
Lauty <lopezlautarojoaquin1@gmail.com>
2021-11-24 12:18:56 -0300

    Refacciones

--- PortalJobs/settings.py
+++ PortalJobs/settings.py
@@ -92,11 +92,11 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portaljobs',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
 
     }
 }
